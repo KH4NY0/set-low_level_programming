d6cd39e28f82dfb9ab1a527fcf6f0bb3e1708422
KH4NY0 <khanyoradebe46@gmail.com>
2026-02-23 12:01:05 -0800

    Project 2 Week 1 Complete

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+1-last_digit.c char c;
+
+1-last_digit.c for (c = 'a'; c <= 'z'; c++)
+1-last_digit.c 1-last_digit.c putchar(c);
+1-last_digit.c putchar('\n');
+
+1-last_digit.c return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+int n;
+char c;
+
+for (n = 0; n < 10; n++)
+putchar(n + '0');
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+
+return (0);
+}
