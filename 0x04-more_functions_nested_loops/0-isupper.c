bdf23e61e6387e5c9cd22642d3f20c75d018d1aa
root <khanyoradebe46@gmail.com>
2026-03-08 04:55:11 -0700

    Week 3 Project 1 Done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+main.h if (c >= 'A' && c <= 'Z')
+main.h main.h return (1);
+main.h return (0);
+}
