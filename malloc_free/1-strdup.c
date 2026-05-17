dd1deeb61a4cfc41a1093a74ba8e1137753d3bb8
KH4NY0 <khanyoradebe46@gmail.com>
2026-05-17 15:49:01 -0700

    Fix 1-strdup and update main.h

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,27 +9,27 @@
  */
 char *_strdup(char *str)
 {
-0-create_array.c char *dup;
-0-create_array.c int len;
-0-create_array.c int i;
+char *dup;
+int len;
+int i;
 
-0-create_array.c if (str == NULL)
-0-create_array.c 0-create_array.c return (NULL);
+if (str == NULL)
+return (NULL);
 
-0-create_array.c len = 0;
-0-create_array.c while (str[len] != '\0')
-0-create_array.c 0-create_array.c len++;
+len = 0;
+while (str[len] != '\0')
+len++;
 
-0-create_array.c dup = malloc(sizeof(char) * (len + 1));
-0-create_array.c if (dup == NULL)
-0-create_array.c 0-create_array.c return (NULL);
+dup = malloc(sizeof(char) * (len + 1));
+if (dup == NULL)
+return (NULL);
 
-0-create_array.c i = 0;
-0-create_array.c while (i <= len)
-0-create_array.c {
-0-create_array.c 0-create_array.c dup[i] = str[i];
-0-create_array.c 0-create_array.c i++;
-0-create_array.c }
+i = 0;
+while (i <= len)
+{
+dup[i] = str[i];
+i++;
+}
 
-0-create_array.c return (dup);
+return (dup);
 }
